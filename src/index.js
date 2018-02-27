647ebc245a920e3c22330002398cc7834816381c
Arseniya <goldstandart165@yandex.ru>
2018-02-27 20:29:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var array = equation.split(' ');
+  var solution = [];
+  var a = array[0];
+  var b = parseInt( array[3]+array[4] );
+  var c = parseInt( array[7]+array[8] );
+  solution.push( Math.round((-b + Math.pow(Math.pow(b, 2) - 4 * a * c, 1/2)) / (2 * a) ) );
+  solution.push( Math.round((-b - Math.pow(Math.pow(b, 2) - 4 * a * c, 1/2)) / (2 * a) ) );
+  solution.sort(function(a, b) {
+  return a - b;
+  });
+  return solution;
 }
